e3845fef46365a1ff4a6febb61ce687376d588b2
cdartora <carlosdalsoler@gmail.com>
2022-03-20 16:49:47 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
+const app = express(); // inicia o express
+app.use(bodyParser.json()); // middleware para traduzir o body das requisições
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
